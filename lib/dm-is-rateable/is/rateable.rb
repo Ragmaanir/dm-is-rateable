2de796ca5d858d93648edf12b58543d4e97eee8b
ragmaanir <ragmaanir@gmail.com>
2014-03-09 18:42:28 +0100

    Extracted helper into separate file

--- lib/dm-is-rateable/is/rateable.rb
+++ lib/dm-is-rateable/is/rateable.rb
@@ -1,5 +1,6 @@
 require 'dm-is-rateable/is/rating'
 require 'dm-is-rateable/is/rateable_extensions'
+require 'dm-is-rateable/is/helper'
 
 module DataMapper
   module Is
@@ -15,100 +16,6 @@ module DataMapper
       class ImpossibleRatingValue < DmIsRateableException; end
 
       Inflector = DataMapper::Inflector
-
-      module Helper
-
-        def self.key_name_from(model_name)
-          model_name.demodulize.underscore + '_id'
-        end
-
-        def self.deduce_rating_type(allowed_ratings)
-          case allowed_ratings
-            when Range then
-              case allowed_ratings.first
-                when Fixnum then [Integer, {:min => allowed_ratings.min, :max => allowed_ratings.max}]
-                when Float then [Float, {:min => allowed_ratings.min, :max => allowed_ratings.max}]
-                else String
-              end
-            when Array then
-              require 'dm-types'
-              DataMapper::Property::Enum[*allowed_ratings]
-            else
-              raise ImpossibleRatingType, "#{allowed_ratings.class} is no supported rating type"
-          end
-        end
-
-        #
-        #
-        def self.infer_rater(by_option)
-          result = case by_option
-            when Symbol
-              name = by_option.to_s.singularize
-              {
-                :name => name.to_sym,
-                :model => name.classify,
-                :key => Inflector.foreign_key(name).to_sym
-              }
-            when Hash
-              by_option.assert_valid_keys(:name,:key,:type,:model,:options)
-              name = by_option[:name].to_s
-              key = by_option[:key] || Inflector.foreign_key(name).to_sym
-              model = key.to_s.gsub(/_id/,'').classify
-              {
-                :model => model,
-                :key => key
-              }.merge(by_option)
-            when DataMapper::Model
-              name = by_option.name.downcase
-              {
-                :name => name.to_sym,
-                :model => by_option.name,
-                :key => Inflector.foreign_key(name).to_sym
-              }
-            when nil then raise ":by option missing"
-            else raise "invalid value for :by: #{by_option}"
-          end
-
-          result = {:type => Integer}.merge(result)
-          result = { :options => { :required => true, :min => 0 } }.merge(result) if result[:type] == Integer
-          result
-        end
-
-        #
-        # Fills in defaults and adds values based on values passed in the options hash.
-        #
-        def self.complete_options(options,clz)
-          raise("model option not supported") if options[:model]
-          raise 'options :as and :concerning are mutually exclusive' if options[:as] and options[:concerning]
-          rater = Helper.infer_rater(options[:by])
-
-          concern = options[:concerning].to_s || ''
-          rating_name = "#{concern.camelize}Rating"
-          rateable_name = clz.name
-
-          model = options[:model] || "#{rater[:model]}#{rateable_name}#{rating_name}"
-          as = options[:as]
-          as ||= if options[:model]
-              model.underscore.pluralize.gsub("#{rateable_name.underscore}_",'')
-            else
-              #"ratings_by_#{rater[:name].to_s.pluralize}".to_sym
-              concern_part = concern.blank? ? '' : "_#{concern}"
-              "#{rater[:name]}#{concern_part}_ratings"
-            end.to_sym
-
-          options = {
-            :with => 0..5,
-            :by => rater,
-            :as => as,
-            :timestamps => true,
-            :model => model,
-            :rating_name => rating_name
-          }.merge(options)
-
-          options.freeze
-        end
-
-      end#Helper
 
       # is :rateable, :by => :users, :with => 0..5, :as => :user_ratings, :model => 'SpecialUserRating' do
       #   def xyz; true; end
